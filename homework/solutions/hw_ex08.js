9050069ed774234617c447fdda1525af451892d4
Moritz Neeb <nt4u@kpvn.de>
2024-03-15 14:40:07 +0100

    add note about exercise 8

---
+++ homework/solutions/hw_ex08.js
@@ -0,0 +1,3 @@
+// This exercise doesn't have a solution. Please check the file 08_dungeon.js, which in a sense already is a "solution".
+// The tasks are more abour reading code and then adapting it in an open ended way.
+// If you have any questions though, don't hesitate to reach out to your teacher.
